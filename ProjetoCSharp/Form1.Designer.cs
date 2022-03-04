ac3ff397c72907b00978169e827bcd461a8b593f
lndsilva <lndsilva@hotmail.com>
2022-03-04 11:55:43 -0300

    Add project files.

---
+++ ProjetoCSharp/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace ProjetoCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnResposta = new System.Windows.Forms.Button();
+            this.lblResposta = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnResposta
+            // 
+            this.btnResposta.Location = new System.Drawing.Point(224, 182);
+            this.btnResposta.Name = "btnResposta";
+            this.btnResposta.Size = new System.Drawing.Size(265, 71);
+            this.btnResposta.TabIndex = 0;
+            this.btnResposta.Text = "Resposta";
+            this.btnResposta.UseVisualStyleBackColor = true;
+            this.btnResposta.Click += new System.EventHandler(this.btnResposta_Click);
+            // 
+            // lblResposta
+            // 
+            this.lblResposta.AutoSize = true;
+            this.lblResposta.Location = new System.Drawing.Point(323, 330);
+            this.lblResposta.Name = "lblResposta";
+            this.lblResposta.Size = new System.Drawing.Size(0, 13);
+            this.lblResposta.TabIndex = 1;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(564, 182);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(196, 71);
+            this.btnLimpar.TabIndex = 2;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lblResposta);
+            this.Controls.Add(this.btnResposta);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnResposta;
+        private System.Windows.Forms.Label lblResposta;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
